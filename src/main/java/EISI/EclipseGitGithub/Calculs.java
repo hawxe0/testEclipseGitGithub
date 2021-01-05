113e41b1dbab16e0fa8df1eedafdfa724745b78c
Alexandre Vandaele <hawxe@protonmail.com>
2021-01-05 16:56:07 +0100

    commit WIP

--- src/main/java/EISI/EclipseGitGithub/Calculs.java
+++ src/main/java/EISI/EclipseGitGithub/Calculs.java
@@ -1,4 +1,4 @@
-package p1;
+package EISI.EclipseGitGithub;
 
 public class Calculs {
 	private final int operande1;
--- src/test/java/EISI/EclipseGitGithub/CalculsTest.java
+++ src/test/java/EISI/EclipseGitGithub/CalculsTest.java
@@ -1,11 +1,9 @@
-package p2;
+package EISI.EclipseGitGithub;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 //import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
-
-import p1.Calculs;
 
 public class CalculsTest {
 
